3073af3f09a253cfc1a9a7cd7013f7e2d5b5a788
itggot-Lukas-Schiavone <lukas.schiavone@itggot.se>
2018-05-18 14:03:05 +0200

    Fixade layouten för menyhemsidan och implementerade ett javascript

---
+++ site/js/script.js
@@ -0,0 +1,10 @@
+function showPizza(pizzaname) {
+    var pizzapictures = document.querySelectorAll(".pizzapicture");
+    var i = 0;
+    while (i < pizzapictures.length) {
+        pizzapictures[i].classList.add("hidden");
+        i += 1;
+    }
+    var selectedpizza = document.querySelector(".pizzapicture." + pizzaname);
+    selectedpizza.classList.remove("hidden");name
+}
